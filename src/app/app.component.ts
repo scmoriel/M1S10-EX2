ad40bbf7c01b04afaadeed5e6f26e489548d1aad
Moriel da Silva Costa <morielsc1997@gmail.com>
2023-04-11 23:43:21 -0300

    Exercicio 2 da Semana 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'M1S10-Ex2';
+  arrayFotos: any = [
+    {
+       url: "https://upload.wikimedia.org/wikipedia/pt/0/00/Iron_Man_poster.jpg",
+       description:"Homem de Ferro"
+     },
+     {
+       url: "https://upload.wikimedia.org/wikipedia/pt/1/1b/Batman_begins.jpg",
+       description:"Batman Begins"
+     },
+     {
+       url: "https://upload.wikimedia.org/wikipedia/pt/6/69/The_Avengers_Cartaz.jpg?20120429164328",
+       description:"Os Vingadores"
+     },
+     {
+       url: "https://upload.wikimedia.org/wikipedia/pt/e/e8/Captain_America_The_Winter_Soldier.jpg",
+       description:"Capitão America 2"
+     },
+     {
+       url: "https://upload.wikimedia.org/wikipedia/pt/b/b2/Guardiansgalaxyposter.jpg",
+       description:"Guardiões da Galáxia"
+     }
+   ]
 }
